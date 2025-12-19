826200e2257cf5540204841392e76798fa006afc
Joseph William Dewdney <josephdewdney@gmail.com>
2025-12-19 22:28:35 +0100

    Implement init command

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,7 +1,36 @@
-const command = process.argv[2]
+import { mkdir } from "node:fs/promises";
 
-if (command === 'init') {
-  console.log('init not implemented')
+const command = Bun.argv[2];
+
+if (command === "init") {
+  await init();
 } else {
-  console.log('usage: seren init')
+  console.log("usage: seren init");
+}
+
+async function init() {
+  await Bun.write(
+    "package.json",
+    JSON.stringify(
+      {
+        name: "my-project",
+        private: true,
+        workspaces: ["apps/*", "packages/*"],
+      },
+      null,
+      2
+    )
+  );
+
+  await mkdir("apps", { recursive: true });
+  await mkdir("packages", { recursive: true });
+
+  await Bun.write(
+    ".gitignore",
+    `.DS_Store
+node_modules
+`
+  );
+
+  console.log("Created monorepo");
 }
