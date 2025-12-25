88447aabd5e0e973ff58a244810a71fd95c1af14
Joseph William Dewdney <josephdewdney@gmail.com>
2025-12-25 14:14:21 +0100

    Pass schema to drizzle for relational queries

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -377,8 +377,9 @@ async function addDbPackage() {
     `${dir}/src/index.ts`,
     `import "dotenv/config";
 import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
 
-export const db = drizzle(process.env["DATABASE_URL"]!);
+export const db = drizzle(process.env["DATABASE_URL"]!, { schema });
 `
   );
 
